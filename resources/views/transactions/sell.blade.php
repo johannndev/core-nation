07b3b0f546529c159e46a0b6387eb2aa300d9dfd
johan sinaga <johanfransiskussinaga@gmail.com>
2024-06-26 15:58:43 +0700

    cek tombol

--- resources/views/transactions/sell.blade.php
+++ resources/views/transactions/sell.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.layout>
 
-    <div id="enterKey"  class="hidden fixed right-2 bottom-5 z-10">
+    <div id="enterKey"  class=" fixed right-2 bottom-5 z-10">
 
         <button type="button"  id="virtualEnterKay" data-name="halo" class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
