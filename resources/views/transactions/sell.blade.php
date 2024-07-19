f878f4301696861ef85d1fe4facd5c39f94759ec
johan sinaga <johanfransiskussinaga@gmail.com>
2024-07-19 14:17:56 +0700

    update transaction

--- resources/views/transactions/sell.blade.php
+++ resources/views/transactions/sell.blade.php
@@ -230,7 +230,7 @@
                                             <input type="text" name="addMoreInputFields[0][itemId]"  id="id0"  placeholder=""  aria-valuetext="0" aria-label="id" hidden/>
                                 
                                             <label for="code" class="block mb-2 text-sm font-medium text-gray-900 ">Code</label>
-                                            <input type="search" data-input="value" data-name="code" data-id="0" onkeydown="return handleCode(event,0)" name="addMoreInputFields[0][code]"  id="code0" class="register_form bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" inputmode="tel" />
+                                            <input type="search" data-input="value" data-name="code" data-id="0" onkeydown="return handleCode(event,0)" name="addMoreInputFields[0][code]"  id="code0" class="register_form bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" inputmode="search" />
 
                                         </div>
 
