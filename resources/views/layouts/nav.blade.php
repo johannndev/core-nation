3818dc83b31e5461bc91f9c0dce6dea5912c4448
johan sinaga <johanfransiskussinaga@gmail.com>
2024-07-19 14:27:04 +0700

    update transaction

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -37,11 +37,11 @@
         </button>
         <a href="https://flowbite.com" class="flex items-center justify-between mr-4">
           <img
-            src="https://flowbite.s3.amazonaws.com/logo.svg"
+            src="{{asset('img/logo.png')}}"
             class="mr-3 h-8"
             alt="Flowbite Logo"
           />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CoreNation</span>
         </a>
 
         <div class="hidden md:block md:pl-2">
