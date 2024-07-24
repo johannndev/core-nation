b83d6b93182e5b8cd0e845b5ac220058b4c3e846
johan sinaga <johanfransiskussinaga@gmail.com>
2024-07-24 13:24:28 +0700

    updete

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -8,7 +8,7 @@
     <ul class="space-y-2">
         <li>
         <a
-            href="#"
+            href="{{route('dashboard')}}"
             class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
         >
             <svg
@@ -21,7 +21,7 @@
             <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
             <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
             </svg>
-            <span class="ml-3">Overview</span>
+            <span class="ml-3">Dashboard</span>
         </a>
         </li>
 
