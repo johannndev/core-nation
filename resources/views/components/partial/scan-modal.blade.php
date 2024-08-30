68fbbd822aaf58bc0110c2cd26aabfa5658f2643
johan sinaga <johanfransiskussinaga@gmail.com>
2024-08-30 10:47:46 +0700

    update

--- resources/views/components/partial/scan-modal.blade.php
+++ resources/views/components/partial/scan-modal.blade.php
@@ -1,9 +1,47 @@
 <div id="modalManual" tabindex="-1" aria-hidden="true" class="fixed left-0 right-0 top-0 bottom-0 z-50 hidden h-full w-full overflow-y-auto overflow-x-hidden  md:inset-0">
     <div class="relative h-full w-full ">
         <!-- Modal content -->
-        <div class="relative rounded-lg bg-white shadow dark:bg-gray-700 h-full w-full flex justify-center">
+        <div class="relative rounded-lg bg-white shadow dark:bg-gray-700 h-full w-full ">
 
-            <div class="w-full md:w-1/3 ">
+            <div class="relative">
+
+                <div class="absolute top-0 right-0 z-30">
+                    <button
+                        id="closeModalButton"
+                        type="button"
+                        class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                        <svg
+                            class="h-3 w-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 14 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                            />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+
+                    <div id="alert-scan" class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                        
+                    </div>
+
+                    
+                </div>
+
+                <div id="camera" style="width:100%"></div>
+
+            </div>
+           
+            {{--  --}}
+            {{-- <div class="w-full md:w-1/3 ">
                 <!-- Modal header -->
                 <div
                     class="flex items-start justify-between rounded-t border-b p-5 dark:border-gray-600"
@@ -65,7 +103,7 @@
                 </div>
              
 
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
--- resources/views/layouts/js/jsutama.blade.php
+++ resources/views/layouts/js/jsutama.blade.php
@@ -19,7 +19,7 @@
                 width:  {min:300},
                 height:  {min:10, max:300} ,
                 facingMode: "environment",
-                aspectRatio: { min: 1, max: 1 }
+                aspectRatio: { min: 1, max: 2 }
             }
         },
         decoder: {
