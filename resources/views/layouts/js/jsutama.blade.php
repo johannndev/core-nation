5f4e8020232247afa8a967aaebfee115cc51c004
johan sinaga <johanfransiskussinaga@gmail.com>
2024-08-30 10:49:16 +0700

    update

--- resources/views/layouts/js/jsutama.blade.php
+++ resources/views/layouts/js/jsutama.blade.php
@@ -17,7 +17,7 @@
             type: "LiveStream",
             constraints: {
                 width:  {min:300},
-                height:  {min:10, max:300} ,
+                height:  {min:300} ,
                 facingMode: "environment",
                 aspectRatio: { min: 1, max: 2 }
             }
