8a39ce050f11deb2eac1cf6868c3206431c32551
johan sinaga <johanfransiskussinaga@gmail.com>
2024-09-24 22:18:02 +0700

    update role permissions

--- config/permission.php
+++ config/permission.php
@@ -36,7 +36,7 @@ return [
          * default value but you may easily change it to any table you like.
          */
 
-        'roles' => 'roles',
+        'roles' => 'aria_roles',
 
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
@@ -44,7 +44,7 @@ return [
          * default value but you may easily change it to any table you like.
          */
 
-        'permissions' => 'permissions',
+        'permissions' => 'aria_permissions',
 
         /*
          * When using the "HasPermissions" trait from this package, we need to know which
