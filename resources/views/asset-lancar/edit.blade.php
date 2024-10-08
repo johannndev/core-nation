0c1a0451d2d182645b55ba4f494f91c3252fb36e
johan sinaga <johanfransiskussinaga@gmail.com>
2024-10-08 14:26:21 +0700

    update

--- resources/views/asset-lancar/edit.blade.php
+++ resources/views/asset-lancar/edit.blade.php
@@ -76,7 +76,7 @@
 
                             <div class="col-span-2">
                                 <label for="description2" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NB</label>
-                                <textarea name="description2" id="description2" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >{{old('description2',$item->description)}}</textarea>
+                                <textarea name="description2" id="description2" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >{{old('description2',$item->printDescription2())}}</textarea>
 
 
                             </div>
