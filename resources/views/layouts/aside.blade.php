109aca1ad428471a56348693404641e25c371bd6
johan sinaga <johanfransiskussinaga@gmail.com>
2024-12-05 11:13:09 +0700

    update image dan note

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -951,7 +951,7 @@
                         <a
                             href="{{route('report.cash')}}"
                             class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                            >Nett Cash</a
+                            >Nett Cash Sby</a
                         >
                     </li>
 
