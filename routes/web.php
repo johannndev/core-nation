b374f76cbbf801e19ace62b8c82a8508df3128e0
johan sinaga <johanfransiskussinaga@gmail.com>
2025-02-22 16:15:52 +0700

    update store location

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,8 @@ use Illuminate\Support\Facades\Hash;
 |
 */
 
-Route::get('jub/order', [ApiJubelioController::class, 'order']);
-Route::get('jub/retur', [ApiJubelioController::class, 'retur']);
+Route::post('jub/order', [ApiJubelioController::class, 'order']);
+Route::post('jub/retur', [ApiJubelioController::class, 'retur']);
 
 Route::get('/', function () {
     return redirect()->route('dashboard');
