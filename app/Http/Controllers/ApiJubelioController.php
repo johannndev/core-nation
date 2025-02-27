457147060646849a37669c7be057abc27ea110f8
johan sinaga <johanfransiskussinaga@gmail.com>
2025-02-27 18:16:06 +0700

    store sell dari jubelio

--- app/Http/Controllers/ApiJubelioController.php
+++ app/Http/Controllers/ApiJubelioController.php
@@ -187,7 +187,7 @@ class ApiJubelioController extends Controller
 
         return response()->json([
             'status' => 'ok',
-            'data' => $dataCollect,
+            'data' => $createData,
             'status_jubelio' => $dataApi['status'],
             'pesan' => 'Transaksi berhasil dikirim ke aria',
             'total_matched' => $matched,
