a0a98c3a2c94591351ad66e3d427b6767c543b96
johan sinaga <johanfransiskussinaga@gmail.com>
2025-04-08 17:19:31 +0700

    update adjust jubelio

--- resources/views/transactions/detail-jubelio-sync.blade.php
+++ resources/views/transactions/detail-jubelio-sync.blade.php
@@ -180,7 +180,7 @@
                                     @elseif ($status === 0)
                                         <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">Tidak Ada</span>
                                     @elseif ($status > 0)
-                                        <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">{{ $item->jubelio_item_id }}</span>
+                                        <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">{{ $status }}</span>
                                     @endif
                                     
 
