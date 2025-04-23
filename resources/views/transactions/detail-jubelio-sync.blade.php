fa502ce69cbbe49f409e63809fcbd6d023597d6e
johan sinaga <johanfransiskussinaga@gmail.com>
2025-04-23 14:43:26 +0700

    adjust sync

--- resources/views/transactions/detail-jubelio-sync.blade.php
+++ resources/views/transactions/detail-jubelio-sync.blade.php
@@ -250,7 +250,7 @@
                         <span class="font-bold">{{ $JubelioA }}</span> 
                         di jubelio
                         @if ($data->a_submit_by)
-                            , Disubmit oleh <span class="font-bold">{{ $data->submitByA->username }}</span>, Adjustmen ID <span class="font-bold">{{ $data->a_reference_id }}</span>
+                            - Disubmit oleh <span class="font-bold">{{ $data->submitByA->username }}</span>, Adjustmen ID <span class="font-bold">{{ $data->a_reference_id }}</span>
                         @endif
                     </p>
                 </div>
