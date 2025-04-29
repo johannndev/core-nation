57276a786d7fd302b903e4a4f7da126707cf74ff
johan sinaga <johanfransiskussinaga@gmail.com>
2025-04-29 12:02:17 +0700

    sell batch udpate

--- resources/views/transactions/detail.blade.php
+++ resources/views/transactions/detail.blade.php
@@ -370,7 +370,7 @@
                         @else
 
                         <a href="{{ route('transaction.genereteInvoice', $data->id) }}" class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg">
-                           Generete PDF
+                            Generate  PDF
                         </a>
 
 
