5b293bbaf188a0a0cd32df500f99664d8c0f43dc
ariantog <arianto.gunawan@gmail.com>
2025-05-09 10:26:15 +0700

    Update ExportController.php
    paginate to 300

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -33,7 +33,7 @@ class ExportController extends Controller
             });
         }
         
-        $dataList = $dataList->paginate(20)->withQueryString();
+        $dataList = $dataList->paginate(300)->withQueryString();
 
 		// dd($dataList);
 
