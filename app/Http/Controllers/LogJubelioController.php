71929138fdd9e105e13ed3debb4a1b2947293c57
ariantog <arianto.gunawan@gmail.com>
2025-05-22 22:15:36 +0700

    Update LogJubelioController.php

--- app/Http/Controllers/LogJubelioController.php
+++ app/Http/Controllers/LogJubelioController.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Database\QueryException;
-use Illuminate\Support\Facades\Auth;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
