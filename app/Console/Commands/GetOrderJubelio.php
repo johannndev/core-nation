4200661312118600add541ce4a06f3ebdef6cdab
johan sinaga <johanfransiskussinaga@gmail.com>
2025-05-23 13:06:53 +0700

    update

--- app/Console/Commands/GetOrderJubelio.php
+++ app/Console/Commands/GetOrderJubelio.php
@@ -129,13 +129,13 @@ class GetOrderJubelio extends Command
 
                             
 
-                            // Crongetorderdetail::where('get_order_id', $data->id)
-                            // ->whereNotIn('status', ['SHIPPED', 'COMPLETED']) 
-                            // ->delete();
+                            Crongetorderdetail::where('get_order_id', $data->id)
+                            ->whereNotIn('status', ['SHIPPED', 'COMPLETED']) 
+                            ->delete();
 
-                            // Crongetorderdetail::where('get_order_id', $data->id)
-                            // ->where('is_canceled', 'Y')
-                            // ->delete();
+                            Crongetorderdetail::where('get_order_id', $data->id)
+                            ->where('is_canceled', 'Y')
+                            ->delete();
 
                             // dd($data->step);
 
