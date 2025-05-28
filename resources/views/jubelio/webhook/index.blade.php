fc36fc4f734f58018c394f53b82221cd4847c996
johan sinaga <johanfransiskussinaga@gmail.com>
2025-05-28 15:37:08 +0700

    cron webhook

--- resources/views/jubelio/webhook/index.blade.php
+++ resources/views/jubelio/webhook/index.blade.php
@@ -210,7 +210,7 @@
 
                                         </td>
                                         <td class="px-4 py-3 ">
-                                            @if ($item->status == 0 || $item->status == 1)
+                                            @if ($item->status == 0 || $item->status == 1 || $item->error)
                                                 <button id="dropdownMore-{{$item->id}}" data-dropdown-toggle="dropdownM-{{$item->id}}" class=" inline-flex items-center  text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg px-3 py-2 text-xs me-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">More <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                                                         </svg>
