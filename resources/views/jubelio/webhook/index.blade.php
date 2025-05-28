50761dbbcffed6903721858adc1d363636cc81aa
johan sinaga <johanfransiskussinaga@gmail.com>
2025-05-28 10:23:33 +0700

    cron webhook

--- resources/views/jubelio/webhook/index.blade.php
+++ resources/views/jubelio/webhook/index.blade.php
@@ -159,6 +159,8 @@
                                                 
                                             @endif
 
+                                            {{$item->error}}
+
                                         </td>
                                         <td class="px-4 py-3 flex">
                                             <a href="{{route('jubelio.webhook.detail',$item->id)}}" class=" items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 me-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-primary-800">
