060b430131300bcb643987c6279684b67fa9f1a4
johan sinaga <johanfransiskussinaga@gmail.com>
2025-05-28 14:43:16 +0700

    cron webhook

--- resources/views/jubelio/webhook/index.blade.php
+++ resources/views/jubelio/webhook/index.blade.php
@@ -226,11 +226,7 @@
                                                         <a href="{{ route('jubelio.webhook.createManual',$item->id) }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Create Manual</a>
                                                         </li>
                                                         <li>
-                                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                                                        </li>
-                                                        <li>
-                                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                                                        </li>
+                                                       
                                                     </ul>
                                                 </div>   
                                          
