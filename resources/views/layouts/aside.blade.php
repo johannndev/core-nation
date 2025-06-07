7d27ab66964a0c1b18de64de941eeeb789e263a7
ariantog <arianto.gunawan@gmail.com>
2025-06-07 20:36:39 +0700

    Update aside.blade.php

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,5 +1,5 @@
 <aside
-    class="print:hidden print:w-0 fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="overflow-scroll print:hidden print:w-0 fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     aria-label="Sidenav"
     id="drawer-navigation"
 >
