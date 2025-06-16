e9d018bd1dfef3630fd64a06ee416dc5b6328312
johan sinaga <johanfransiskussinaga@gmail.com>
2025-06-16 14:52:13 +0700

    po move

--- resources/views/transactions/po-detail.blade.php
+++ resources/views/transactions/po-detail.blade.php
@@ -328,7 +328,9 @@
 
                                 </th>
 
-                                <td class="barcode-col  px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 print:whitespace-normal print:break-words  whitespace-nowrap dark:text-white">
+                                @isset($itemTd->item)
+
+                                  <td class="barcode-col  px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 print:whitespace-normal print:break-words  whitespace-nowrap dark:text-white">
                                     {{$itemTd->item->id ?? ''}}
                                 </td>
 
@@ -492,6 +494,169 @@
                                 </td>
 
                                 <x-transaction.warehouse-item  :idItem="$idItem"/>
+                                    
+                                @endisset
+
+                                @empty($itemTd->item)
+                                    
+                             
+
+                                <td class="barcode-col  px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 print:whitespace-normal print:break-words  whitespace-nowrap dark:text-white">
+                                   
+                                </td>
+
+                                <td class="sku-col hidden px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 print:whitespace-normal print:break-words  whitespace-nowrap dark:text-white">
+                                    
+                                </td>
+
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 print:whitespace-normal print:break-words  whitespace-nowrap dark:text-white">
+                                    
+                                </td>
+                               
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 print:whitespace-normal print:break-words  whitespace-normal max-w-40 dark:text-white">
+                                    <p class="min-w-40 print:min-w-0 print:whitespace-normal print:break-words "></p>
+                                </td>
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900  whitespace-normal dark:text-white">
+                                    <p class="min-w-40 print:min-w-0  print:whitespace-normal print:break-words "></p>
+                                    
+                                </td>
+                                <td class="sku-col hidden px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    
+                                </td>
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                   
+                                </td>
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    
+                                    @if ($itemTd->available_quantity > 0)
+
+                                        {{$itemTd->available_quantity}}
+
+                                    @else
+
+                                        @if ($itemTd->status == 1)
+
+                                              <!-- Modal toggle -->
+                                            <button data-modal-target="invoice-model-{{$itemTd->id}}" data-modal-toggle="invoice-model-{{$itemTd->id}}" class="text-blue-500 hover:text-blue-600 hover:underline  font-medium " type="button">
+                                               Update
+                                             </button>
+                                             
+                                             <!-- Main modal -->
+                                             <div id="invoice-model-{{$itemTd->id}}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                                 <div class="relative p-4 w-full max-w-md max-h-full">
+                                                     <!-- Modal content -->
+                                                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                         <!-- Modal header -->
+                                                         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                                             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                                                 Update <span class="font-bold">{{$itemTd->item->code}}</span>
+                                                             </h3>
+                                                             <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="invoice-model-{{$itemTd->id}}">
+                                                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                                 </svg>
+                                                                 <span class="sr-only">Close modal</span>
+                                                             </button>
+                                                         </div>
+ 
+                                                         <form action="{{route('transaction.PoUpdateItemQty',$itemTd->id)}}" method="post" class="space-y-4" id="form-update">
+                                                         <!-- Modal body -->
+                                                         @csrf
+ 
+                                                         @method('PATCH')
+ 
+                                                         <div class="p-4 md:p-5">
+                                                             
+                                                             <div>
+                                                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Available Quantity</label>
+                                                                 <input  type="number"  name="qty" value="1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                                                             </div>
+ 
+                                                             
+                                                                 
+                                                                 
+                                                                 
+                                                         
+                                                         </div>
+
+                                                        </form>
+
+                                                        <div class="flex justify-between p-4 md:p-5 border-t rounded-b border-gray-200  dark:border-gray-600">
+
+                                                            <div>
+                                                                <div class="flex items-center  ">
+                                                                    <button id="btn-update" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                                                    <button data-modal-hide="invoice-model-{{$itemTd->id}}" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                                                                </div>
+
+                                                            </div>
+
+                                                        
+
+                                                            <div>
+
+                                                                <button id="btn-kosong" class="px-5 py-2.5 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                                                    Kosong
+                                                                </button>
+
+                                                                
+
+                                                            </div>
+
+                                                        </div>
+ 
+                                                        
+ 
+                                                       
+                                                     </div>
+
+                                                     
+                                                     <form action="{{route('transaction.updateItemKosong',$itemTd->id)}}" method="post" id="form-kosong">
+
+                                                        @csrf
+                                                        @method('PATCH')
+
+                                                       
+            
+                                                       
+                                                    </form>
+
+                                                 </div>
+                                             </div> 
+ 
+                                        
+                                        @else
+                                            {{$itemTd->available_quantity}}
+                                        @endif
+                                        
+                                    @endif
+
+                                    
+                                </td>
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    
+                                </td>
+
+                               
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  
+                                </td>
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  
+                                </td>
+
+                                <td class="px-4 py-2 print:px-0 print:py-0 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                   
+                                </td>
+
+                                @endempty
                                
                             </tr>
                           
