e76e6ed3816caff96cc08f97c09f577463015465
johan sinaga <johanfransiskussinaga@gmail.com>
2025-08-06 15:01:36 +0700

    stat

--- app/View/Components/Customer/Stat.php
+++ app/View/Components/Customer/Stat.php
@@ -64,175 +64,112 @@ class Stat extends Component
         $endDate =Carbon::createFromDate($year,$month,'1')->endOfMonth()->toDateString();
 
         
-        $dataCashInCustomer = DB::table('transactions')
-            ->where('date','>=',$startDate)->where('date','<=',$endDate)
-            ->where("type",'=',Transaction::TYPE_CASH_IN)
-            ->where("sender_id",'=',$customer->id)
-            ->where("receiver_type",'=',Customer::TYPE_CUSTOMER)
-            ->sum('total');
-
-        $dataCashInReselle = DB::table('transactions')
-            ->where('date','>=',$startDate)->where('date','<=',$endDate)
-            ->where("type",'=',Transaction::TYPE_CASH_IN)
-            ->where("sender_id",'=',$customer->id)
-            ->where("receiver_type",'=',Customer::TYPE_RESELLER)
-            ->sum('total');
-
-
-        $dataCashOutCustomer = DB::table('transactions')
-            ->where('date','>=',$startDate)->where('date','<=',$endDate)
-            ->where("type",'=',Transaction::TYPE_CASH_IN)
-            ->where("receiver_id",'=',$customer->id)
-            ->where("sender_type",'=',Customer::TYPE_CUSTOMER)
-            ->sum('total');
-
-        $dataCashOutReselle = DB::table('transactions')
-            ->where('date','>=',$startDate)->where('date','<=',$endDate)
-            ->where("type",'=',Transaction::TYPE_CASH_IN)
-            ->where("receiver_id",'=',$customer->id)
-            ->where("sender_type",'=',Customer::TYPE_CUSTOMER)
-            ->sum('total');
-
-        $dataSell = DB::table('transactions')
-            ->where('date','>=',$startDate)->where('date','<=',$endDate)
-            ->where("type",'=',Transaction::TYPE_SELL)
-            ->where("sender_id",'=',$customer->id)
-            ->sum('total');
-
-        $dataReturn = DB::table('transactions')
-            ->where('date','>=',$startDate)->where('date','<=',$endDate)
-            ->where("type",'=',Transaction::TYPE_RETURN)
-            ->where("receiver_id",'=',$customer->id)
-            ->sum('total');
-
-        $data = [
-            'cash_in' => [
-                'customer' => $dataCashInCustomer,
-                'reseller' => $dataCashInReselle,
-                'total' => $dataCashInCustomer + $dataCashInReselle
-            ],
-            'cash_out' => [
-                'customer' => $dataCashOutCustomer,
-                'reseller' => $dataCashOutReselle,
-                'total' => $dataCashOutCustomer + $dataCashOutReselle
-            ],
-            'sell' => [
-                'customer' =>'-',
-                'reseller' => '-',
-                'total' => $dataSell
-            ],
-            'return' => [
-                'customer' =>'-',
-                'reseller' => '-',
-                'total' => $dataReturn
-            ]
-
-        ];
-
-        dd($data);
-            
-
-
-        // dd($startDate, $endDate);
-
         $data = DB::table('transactions')
             ->selectRaw("
-                -- Cash In
                 SUM(CASE 
-                    WHEN type = ? AND sender_id = ? THEN total ELSE 0 END) as cash_in_customer,
+                    WHEN type = ? AND sender_id = ? AND receiver_type = ? THEN total 
+                    ELSE 0 
+                END) as cash_in_customer,
                 SUM(CASE 
-                    WHEN type = ? AND sender_id = ? THEN total ELSE 0 END) as cash_in_reseller,
-                
-                -- Cash Out
-                SUM(CASE 
-                    WHEN type = ? AND receiver_id = ? THEN total ELSE 0 END) as cash_out_customer,
-                SUM(CASE 
-                    WHEN type = ? AND receiver_id = ? THEN total ELSE 0 END) as cash_out_reseller,
+                    WHEN type = ? AND sender_id = ? AND receiver_type = ? THEN total 
+                    ELSE 0 
+                END) as cash_in_reseller,
 
-                -- Sell Total
                 SUM(CASE 
-                    WHEN type = ? THEN total ELSE 0 END) as sell_total,
+                    WHEN type = ? AND receiver_id = ? AND sender_type = ? THEN total 
+                    ELSE 0 
+                END) as cash_out_customer,
+                SUM(CASE 
+                    WHEN type = ? AND receiver_id = ? AND sender_type = ? THEN total 
+                    ELSE 0 
+                END) as cash_out_reseller,
 
-                -- Return Total
                 SUM(CASE 
-                    WHEN type = ? THEN total ELSE 0 END) as return_total
+                    WHEN type = ? AND sender_id = ? THEN total 
+                    ELSE 0 
+                END) as sell_total,
+
+                SUM(CASE 
+                    WHEN type = ? AND receiver_id = ? THEN total 
+                    ELSE 0 
+                END) as return_total
             ", [
                 // cash_in_customer
-                Transaction::TYPE_CASH_IN, $customer->id,
+                Transaction::TYPE_CASH_IN, $customer->id, Customer::TYPE_CUSTOMER,
                 // cash_in_reseller
-                Transaction::TYPE_CASH_IN, $customer->id,
+                Transaction::TYPE_CASH_IN, $customer->id, Customer::TYPE_RESELLER,
                 // cash_out_customer
-                Transaction::TYPE_CASH_OUT, $customer->id,
+                Transaction::TYPE_CASH_IN, $customer->id, Customer::TYPE_CUSTOMER,
                 // cash_out_reseller
-                Transaction::TYPE_CASH_OUT, $customer->id,
+                Transaction::TYPE_CASH_IN, $customer->id, Customer::TYPE_RESELLER,
                 // sell_total
-                Transaction::TYPE_SELL,
+                Transaction::TYPE_SELL, $customer->id,
                 // return_total
-                Transaction::TYPE_RETURN,
+                Transaction::TYPE_RETURN, $customer->id,
             ])
             ->whereBetween('date', [$startDate, $endDate])
             ->first();
 
-            // Bentuk array hasil
-            $result = [
-                'cash_in' => [
-                    'customer' => (float) $data->cash_in_customer,
-                    'reseller' => (float) $data->cash_in_reseller,
-                    'total' => (float) $data->cash_in_customer + (float) $data->cash_in_reseller,
-                ],
-                'cash_out' => [
-                    'customer' => (float) $data->cash_out_customer,
-                    'reseller' => (float) $data->cash_out_reseller,
-                    'total' => (float) $data->cash_out_customer + (float) $data->cash_out_reseller,
-                ],
-                'sell' => [
-                    'customer' => '-',
-                    'reseller' => '-',
-                    'total' => (float) $data->sell_total,
-                ],
-                'return' => [
-                    'customer' => '-',
-                    'reseller' => '-',
-                    'total' => (float) $data->return_total,
-                ],
-            ];
+        $dataStat = [
+            'cash_in' => [
+                'customer' => $data->cash_in_customer,
+                'reseller' => $data->cash_in_reseller,
+                'total' => $data->cash_in_customer + $data->cash_in_reseller
+            ],
+            'cash_out' => [
+                'customer' => $data->cash_out_customer,
+                'reseller' => $data->cash_out_reseller,
+                'total' => $data->cash_out_customer + $data->cash_out_reseller
+            ],
+            'sell' => [
+                'customer' => null,
+                'reseller' => null,
+                'total' => $data->sell_total
+            ],
+            'return' => [
+                'customer' => null,
+                'reseller' => null,
+                'total' => $data->return_total
+            ]
+        ];
 
-            dd($data);
+        // dd($result);
 
 
-        //2. get sales data
-		$transactionTable = Transaction::table();
-		//2.1 get cash in and cash out
-		if($customer->type == CUSTOMER::TYPE_CUSTOMER || $customer->type == CUSTOMER::TYPE_RESELLER) {
-			$dataCashIn = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_cash_in'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_IN)->where("$transactionTable.sender_id",'=',$customer->id)->first();
-			$dataCashOut = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_cash_out'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_OUT)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
-			$dataSell = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_sell'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_SELL)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
-			$dataReturn = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_return'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_RETURN)->where("$transactionTable.sender_id",'=',$customer->id)->first();
-        } else {
-			$dataCashIn = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_cash_in'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_IN)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
-			$dataCashOut = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_cash_out'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_OUT)->where("$transactionTable.sender_id",'=',$customer->id)->first();
-			$dataSell = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_sell'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_SELL)->where("$transactionTable.sender_id",'=',$customer->id)->first();
-			$dataReturn = DB::table($transactionTable)->select(array(
-				DB::raw('SUM(total) as total_return'),
-			))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_RETURN)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
-        }
+        
+        // //2. get sales data
+		// $transactionTable = Transaction::table();
+		// //2.1 get cash in and cash out
+		// if($customer->type == CUSTOMER::TYPE_CUSTOMER || $customer->type == CUSTOMER::TYPE_RESELLER) {
+		// 	$dataCashIn = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_cash_in'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_IN)->where("$transactionTable.sender_id",'=',$customer->id)->first();
+		// 	$dataCashOut = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_cash_out'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_OUT)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
+		// 	$dataSell = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_sell'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_SELL)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
+		// 	$dataReturn = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_return'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_RETURN)->where("$transactionTable.sender_id",'=',$customer->id)->first();
+        // } else {
+		// 	$dataCashIn = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_cash_in'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_IN)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
+		// 	$dataCashOut = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_cash_out'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_CASH_OUT)->where("$transactionTable.sender_id",'=',$customer->id)->first();
+		// 	$dataSell = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_sell'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_SELL)->where("$transactionTable.sender_id",'=',$customer->id)->first();
+		// 	$dataReturn = DB::table($transactionTable)->select(array(
+		// 		DB::raw('SUM(total) as total_return'),
+		// 	))->where('date','>=',$startDate)->where('date','<=',$endDate)->where("$transactionTable.type",'=',Transaction::TYPE_RETURN)->where("$transactionTable.receiver_id",'=',$customer->id)->first();
+        // }
 
         // dd($dataCashIn->total_cash_in,$dataCashOut);
 
-        return view('components.customer.stat',compact('dataSell','dataReturn','dataCashIn','dataCashOut'));
+        return view('components.customer.stat',compact('dataStat'));
     }
 }
--- resources/views/components/customer/stat.blade.php
+++ resources/views/components/customer/stat.blade.php
@@ -4,6 +4,7 @@
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
+                    <th scope="col" class="px-4 py-3">Type</th>
                     <th scope="col" class="px-4 py-3">Cash In</th>
                     <th scope="col" class="px-4 py-3">Cash Out</th>
                     <th scope="col" class="px-4 py-3">Sell</th>
@@ -11,11 +12,28 @@
                 </tr>
             </thead>
             <tbody>
+                 <tr class="border-b dark:border-gray-700 hover:bg-gray-100">
+                    <td class="px-4 py-3">Customer</td>
+                    <td class="px-4 py-3">{{ number_format($dataStat['cash_in']['customer']) }}</td>
+                    <td class="px-4 py-3">{{ number_format($dataStat['cash_out']['customer']) }}</td>
+                    <td class="px-4 py-3">-</td>
+                    <td class="px-4 py-3">-</td>
+                  
+                </tr>
                 <tr class="border-b dark:border-gray-700 hover:bg-gray-100">
-                    <td class="px-4 py-3">{{ number_format($dataCashIn->total_cash_in) }}</td>
-                    <td class="px-4 py-3">{{ number_format($dataCashOut->total_cash_out) }}</td>
-                    <td class="px-4 py-3">{{ number_format($dataSell->total_sell) }}</td>
-                    <td class="px-4 py-3">{{ number_format($dataReturn->total_return) }}</td>
+                    <td class="px-4 py-3">Reseller</td>
+                    <td class="px-4 py-3">{{ number_format($dataStat['cash_in']['reseller']) }}</td>
+                    <td class="px-4 py-3">{{ number_format($dataStat['cash_out']['reseller']) }}</td>
+                    <td class="px-4 py-3">-</td>
+                    <td class="px-4 py-3">-</td>
+                </tr>
+
+                <tr class="border-b dark:border-gray-700 hover:bg-gray-100">
+                    <th class="px-4 py-3">Total</th>
+                    <th class="px-4 py-3">{{ number_format($dataStat['cash_in']['total']) }}</th>
+                    <th class="px-4 py-3">{{ number_format($dataStat['cash_out']['total']) }}</th>
+                    <th class="px-4 py-3">{{ number_format($dataStat['sell']['total']) }}</th>
+                    <th class="px-4 py-3">{{ number_format($dataStat['return']['total']) }}</th>
                 </tr>
             </tbody>
         </table>
