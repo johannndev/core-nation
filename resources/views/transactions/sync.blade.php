64e2707e4d1ec542130e98bc4b1f8c3b1e56373b
johan sinaga <johanfransiskussinaga@gmail.com>
2025-08-14 15:32:15 +0700

    update

--- resources/views/transactions/sync.blade.php
+++ resources/views/transactions/sync.blade.php
@@ -51,8 +51,8 @@
                                     <div>
                                         <label for="display" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Display</label>
                                         <select id="display" name="display" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                            <option  {{Request('display','N') == 'N' ? 'selected' : ''}} value="N">Hidden</option>
-                                            <option  {{Request('display','N') == 'Y' ? 'selected' : ''}} value="Y">Show</option>
+                                            <option  {{Request('display','N') == 'N' ? 'selected' : ''}} value="N">Show</option>
+                                            <option  {{Request('display','N') == 'Y' ? 'selected' : ''}} value="Y">Hidden</option>
                                         </select>
                                     </div>
 
