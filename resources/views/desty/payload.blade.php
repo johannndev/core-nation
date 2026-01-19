c7c16835e78b230c40565d3b599fee958afaeb2c
johan sinaga <johanfransiskussinaga@gmail.com>
2026-01-19 12:27:53 +0700

    update

--- resources/views/desty/payload.blade.php
+++ resources/views/desty/payload.blade.php
@@ -366,10 +366,10 @@
                                                 <div>
 
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5"
-                                                        stroke="currentColor"class="h-4 w-4 text-gray-500">
+                                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                        class="h-4 w-4 text-gray-500">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                                                            d="M15.75 7.5a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 19.5a7.5 7.5 0 1115 0v.75a.75.75 0 01-.75.75h-13.5a.75.75 0 01-.75-.75v-.75z" />
                                                     </svg>
 
                                                 </div>
