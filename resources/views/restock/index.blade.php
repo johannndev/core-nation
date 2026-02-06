b73796b5c87a7686bfe4821e7b79d6cea3338ca3
johan sinaga <johanfransiskussinaga@gmail.com>
2026-02-06 14:40:17 +0700

    update

--- resources/views/restock/index.blade.php
+++ resources/views/restock/index.blade.php
@@ -198,6 +198,20 @@
                                         <td class="px-4 py-3">
                                             <div class="flex items-center gap-2">
 
+                                                {{-- history --}}
+                                                <a href="{{ route('restock.history', $item->id) }}"
+                                                    title="Lihat History Perubahan Stok"
+                                                    class="inline-flex items-center justify-center w-9 h-9 rounded-lg text-yellow-600 bg-yellow-100 hover:bg-yellow-200 focus:ring-2 focus:ring-yellow-300 dark:bg-yellow-900 dark:text-yellow-300">
+
+                                                    {{-- Clock --}}
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                    </svg>
+                                                </a>
+
                                                 {{-- UPDATE DATA --}}
                                                 <a href="{{ route('restock.update', $item->id) }}"
                                                     title="Update Data Stok"
@@ -227,7 +241,7 @@
                                                     </svg>
                                                 </a>
 
-                                                {{-- history --}}
+                                                
 
 
 
