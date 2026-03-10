957ca97ec484ed0416686440b70074763185fd66
johan sinaga <johanfransiskussinaga@gmail.com>
2026-03-10 14:51:38 +0700

    update

--- resources/views/transactions/detail.blade.php
+++ resources/views/transactions/detail.blade.php
@@ -435,7 +435,7 @@
                                 @if (!$limitDownload || !$isHiddenA)
                                     <a href="{{ route('transaction.exportDesty', [
                                         'id' => $data->id,
-                                        'side' => 'a',
+                                        'side' => 'A',
                                         'data' => $itemDesty['sender'],
                                     ]) }}"
                                         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
@@ -456,7 +456,7 @@
                                 @if (!$limitDownload || !$isHiddenB)
                                     <a href="{{ route('transaction.exportDesty', [
                                         'id' => $data->id,
-                                        'side' => 'b',
+                                        'side' => 'B',
                                         'data' => $itemDesty['receiver'],
                                     ]) }}"
                                         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
@@ -482,7 +482,7 @@
                                 @if (!$limitDownload || !$isHidden)
                                     <a href="{{ route('transaction.exportDesty', [
                                         'id' => $data->id,
-                                        'side' => 'a',
+                                        'side' => 'A',
                                         'data' => $itemDesty,
                                     ]) }}"
                                         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
