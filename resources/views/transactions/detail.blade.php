a2c56c756a675b5926c3c2495c35a3346f2716c0
johan sinaga <johanfransiskussinaga@gmail.com>
2026-03-10 13:38:16 +0700

    desty api

--- resources/views/transactions/detail.blade.php
+++ resources/views/transactions/detail.blade.php
@@ -421,6 +421,19 @@
 
                         @if (isset($cronFlatform['desty_order']) && $cronFlatform['desty_order'] === 1)
 
+                            @if ($data->type == 3)
+                                <a href="{{ route('transaction.exportDesty', ['id' => $data->id, 'data' => $itemDesty['sender']]) }}"
+                                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                                    Desty Sender
+                                </a>
+
+                                <a href="{{ route('transaction.exportDesty', ['id' => $data->id, 'data' => $itemDesty['receiver']]) }}"
+                                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                                    Desty Receiver
+                                </a>
+                            @else
+                            @endif
+
                             @if ($showDesty == 1)
 
                                 {{-- USER TANPA LIMIT DOWNLOAD -> bebas download --}}
