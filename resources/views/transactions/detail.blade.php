078710e3b6c60eb72644afa7c61eb3b4f129dfa6
johan sinaga <johanfransiskussinaga@gmail.com>
2026-03-13 22:30:02 +0700

    update payload

--- resources/views/transactions/detail.blade.php
+++ resources/views/transactions/detail.blade.php
@@ -515,6 +515,20 @@
 
                         @endif
 
+                        @if ($data->submit_type == 1)
+                            <a href="{{ route('transaction.detailDestySync', $data->id) }}"
+                                    class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg">
+                                    <svg class="h-3.5 w-3.5 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                                        <path fill-rule="evenodd"
+                                            d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+
+                                    Desty Adjustment
+                                </a>
+                        @endif
+
 
                         <button type="button" id="deleteButton" data-modal-target="deleteModal"
                             data-modal-toggle="deleteModal"
