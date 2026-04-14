8727c1dff6139f30013e64e04ef5ad0b23e92889
johan sinaga <johanfransiskussinaga@gmail.com>
2026-04-14 15:08:22 +0700

    Add grubItem method and corresponding view for warehouse item report

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -960,6 +960,11 @@
                             <a href="{{ route('report.pembelian') }}"
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Pembelian</a>
                         </li>
+                        
+                        <li>
+                            <a href="{{ route('report.grubItem') }}"
+                                class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Item Gudang</a>
+                        </li>
                     </ul>
                 </li>
             @endif
