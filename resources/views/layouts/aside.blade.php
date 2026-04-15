ae5ef656e7b57b1cfbe56ccf3928313515f5566c
johan sinaga <johanfransiskussinaga@gmail.com>
2026-04-15 11:10:59 +0700

    update

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -965,6 +965,10 @@
                             <a href="{{ route('report.grubItem') }}"
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Item Gudang</a>
                         </li>
+                        <li>
+                            <a href="{{ route('report.laporanBiaya') }}"
+                                class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Laporan Biaya</a>
+                        </li>
                     </ul>
                 </li>
             @endif
