1b094565efe6f7ef16f27f8739f89f776cc6eba8
johan sinaga <johanfransiskussinaga@gmail.com>
2026-05-19 14:58:43 +0700

    update

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -848,6 +848,12 @@
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Jubelio
                                 Log</a>
                         </li>
+
+                        <li>
+                            <a href="{{ route('jubelio.stock-checks.index') }}"
+                                    class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Jubelio
+                                    Stock Check</a>
+                        </li>
                     </ul>
 
                 </li>
